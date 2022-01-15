45439b8db23ff6e67163070465b6b40812124684
= <=>
2022-01-15 18:39:27 +0000

    adding footer and header

--- index.php
+++ index.php
@@ -6,18 +6,13 @@
  * @package Aquila
  */
 
+ get_header();
  ?>
 
- <!DOCTYPE html>
- <html lang="en">
- <head>
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Wordpress</title>
- </head>
- <body>
-     Hello Wordpress
-     
- </body>
- </html>
+     <div class="content">
+        Content
+     </div>
+
+<?php 
+    //include_once 'footer.php';
+    get_footer();
