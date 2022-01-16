625a2e42e280be87b5ab803f6b86334de57ecb30
= <=>
2022-01-16 17:50:56 +0000

    register script y cambiar a register y enqueue

--- functions.php
+++ functions.php
@@ -13,13 +13,32 @@ wp_die();
 */
 
 function aquila_enqueue_scripts(){
-    //wp_enqueue_style('main-css', get_template_directory_uri().'/main.css',['stylesheet'], filemtime(get_template_directory().'/style.css'),'all');
-    //la funcion anterior carga el main.css pero con dependencia
-    //del stylesheet por lo que primero se cargara stylesheet y
-    //despues se cargara main-css
-    //tambien da un numero de version con filemtime y para que screen en este caso 'all'
+    /*
+    wp_enqueue_style('main-css', get_template_directory_uri().'/main.css',['stylesheet'], filemtime(get_template_directory().'/style.css'),'all');
+    la funcion anterior carga el main.css pero con dependencia
+    del stylesheet por lo que primero se cargara stylesheet y
+    despues se cargara main-css
+    tambien da un numero de version con filemtime y para que screen en este caso 'all'
     wp_enqueue_style('stylesheet', get_stylesheet_uri(),[],filemtime(get_template_directory().'/style.css'),'all');
 
+    para incluir script
+    wp_enqueue_script('main-js', get_template_directory_uri().'/assets/main.js',[],filemtime(get_template_directory().'/assets/main.js'), true);
+
+    para hace lo anterior es mejor hacer primero el registro y luegos el enqueue
+    wp_register_style('style-css', get_stylesheet_uri(),[],filemtime(get_template_directory().'/style.css'),'all');
+
+    if(is_archive()){
+        wp_enqueue_style('style-css');
+    }
+    */
+
+    //esta es la mejor manera de hacerlo primero register y despues enqueue
+
+    wp_register_style('style-css', get_stylesheet_uri(),[],filemtime(get_template_directory().'/style.css'),'all');
+    wp_register_script('main-js', get_template_directory_uri().'/assets/main.js',[],filemtime(get_template_directory().'/assets/main.js'), true);
+
+    wp_enqueue_style('style-css');
+    wp_enqueue_script('main-js');
 }
 
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
